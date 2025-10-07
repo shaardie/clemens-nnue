2bfcad7bb238ec5ecb4e2fea556e4eec8af504da
Sven Haardiek <sven@haardiek.de>
2025-10-07 20:56:49 +0200

    Fix typo

--- data_loader.py
+++ data_loader.py
@@ -6,7 +6,7 @@ import numpy as np
 from model import NUM_FEATURES
 
 # External C Library
-libdataset = ctypes.CDLL(".dataset/build/libdataset.so")
+libdataset = ctypes.CDLL("./dataset/build/libdataset.so")
 
 CreateBatchStream = libdataset.CreateBatchStream
 CreateBatchStream.argtypes = [ctypes.c_char_p, ctypes.c_uint, ctypes.c_uint]
@@ -30,7 +30,7 @@ class SparseBatch(ctypes.Structure):
 
     def get_tensors(self, device):
         stm = torch.from_numpy(
-            np.ctypeslib.as_array(self.score, shape=(self.size, 1))
+            np.ctypeslib.as_array(self.stm, shape=(self.size, 1))
         ).to(device)
         score = torch.from_numpy(
             np.ctypeslib.as_array(self.score, shape=(self.size, 1))
